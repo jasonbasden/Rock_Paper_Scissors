250a572533dbb5335775ae15834f2b2e16e4a284
Jason Basden <JasonBasden@ou.edu>
2020-03-28 22:48:40 -0500

    Need to fix the break

--- rps.py
+++ rps.py
@@ -5,27 +5,37 @@ userInput = input("Do you want to play Rock, Paper, Scissors?: ").lower()
 
 while userInput == "yes":
 
-    choices = ["Rock", "Paper", "Scissors"]
-
-    for i in choices:
-        compChoice = i
+    compChoice = random.choice = ["Rock", "Paper", "Scissors"]
 
     userChoice = input("Rock! Paper! Scissors!: ")
 
     if compChoice == userChoice:
         print("It's a tie!")
+        break
     
     if compChoice == "Rock" and userChoice == "Scissors" and compChoice != userChoice:
         print("The computer wins!")
     else:
         print("You win!")
+        break
 
     if compChoice == "Paper" and userChoice == "Rock" and compChoice != userChoice:
         print("The computer wins!")
     else:
         print("You win!")
+        break
 
     if compChoice == "Scissors" and userChoice == "Paper" and compChoice != userChoice:
         print("The computer wins!")
     else:
         print("You win!")
+        break
+
+
+
+    userInput2 = input("Play again? ").lower()
+    
+    if userInput2 == "yes":
+        userInput = "yes"
+    else:
+        userInput = "no"
